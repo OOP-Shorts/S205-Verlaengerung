60eb5067dfd1003d90a3b5f174aa056266d4c546
Alexander Bazo <alexanderbazo@googlemail.com>
2021-12-24 07:59:57 +0100

    Add solution

--- src/Verlaengerung.java
+++ src/Verlaengerung.java
@@ -1,6 +1,21 @@
 public class Verlaengerung {
 
-    public static void main(String[] args) {
+    private static String[] extend(String[] array, String item) {
+        String[] result = new String[array.length + 1];
+        System.arraycopy(array, 0, result, 0, array.length);
+        result[result.length - 1] = item;
+        return result;
+    }
 
+    public static void main(String[] args) {
+        String[] favorites = new String[0];
+        favorites = extend(favorites, "Polarfuchs");
+        favorites = extend(favorites, "Erdmännchen");
+        favorites = extend(favorites, "Wachtel");
+        favorites = extend(favorites, "Eisvogel");
+        favorites = extend(favorites, "Siebenschläfer");
+        for (String animal : favorites) {
+            System.out.println(animal);
+        }
     }
 }
